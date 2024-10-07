6067e65c65b913e84b1dfd1f8bef6a9a44521b14
Rodrigo <rodrigotriboni@outlook.com>
2024-10-07 07:52:59 -0300

    Create MauiAppSemb

---
+++ MauiAppSEMB/MauiAppSEMB/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppSEMB
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MauiAppSEMB/MauiAppSEMB/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppSEMB
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
